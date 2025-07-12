46236373aae52e4371d0d601d485f78a6dab14b1
Nanda Fadhli <38741625+nandasenyo@users.noreply.github.com>
2025-07-12 09:19:02 +0700

    Update app.py

--- app.py
+++ app.py
@@ -25,4 +25,14 @@ if st.button("Say hello"):
 
 st.link_button("Go to gallery", "https://streamlit.io/gallery")
 
-st.image("sunrise.jpg", caption="Sunrise by the mountains")
+with col1:
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
