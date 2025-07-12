cdfd7d04ccdac31ff45edf6568af3830cab9d2aa
Nanda Fadhli <38741625+nandasenyo@users.noreply.github.com>
2025-07-12 09:03:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -12,3 +12,10 @@ st.subheader("_Streamlit_ is :blue[cool] :sunglasses:")
 st.markdown("*Streamlit* is **really** ***cool***.")
 
 st.metric(label="Temperature", value="70 °F")
+
+import pandas as pd
+import numpy as np
+
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+
+st.line_chart(chart_data)
